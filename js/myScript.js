286c343b12b327f1f3ebe346decf2773f969290e
raghuvarunreddypothulavenkata <raghu.pothula@gmail.com>
2015-05-07 13:13:51 -0700

    Changed dae files and some fiile content

--- js/myScript.js
+++ js/myScript.js
@@ -1,24 +1,3 @@
-$(document).on('click', '#btnDownload', function(){
-	$("#results").text("");
-	var ObjToJSON = new Object();
-		ObjToJSON.FirstName=document.getElementById("firstName").value;
-		ObjToJSON.LastName=document.getElementById("lastName").value;
-		ObjToJSON.Email=document.getElementById("email").value;
-	$.ajax({
-		url: $("#myForm").attr("action"),
-		type: "POST",
-		data: JSON.stringify(ObjToJSON),
-		processData: false,
-		contentType: "application/json; charset=UTF-8",
-		complete: function()
-			{
-				$("#results").text("Addition Successful!!!");
-			}
-	});
+$carousel.bind('slide.bs.carousel', function (e) {
+    console.log('slide event!');
 });
-
-$(document).on('click', '#btnCancel', function(){
-	$(location).attr('href','home.html');
-});
-
-$("#myForm").submit(function(){return false;});
